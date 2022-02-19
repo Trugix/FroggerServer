3c9786251e182fce83406319d61ca7e088f81652
E343C <e.cesari@studenti.unibs.it>
2022-02-19 11:43:44 +0100

    WIP

--- src/PnlFrog.java
+++ src/PnlFrog.java
@@ -64,7 +64,7 @@ public class PnlFrog extends JPanel implements KeyListener{
         for (Entity e: entities)
         {
             g2.drawImage(e.sprite, e.p.getX(), e.p.getY(), null);
-              //  g2.draw(e.hitbox); //solo per vedere l'hitbox
+            g2.draw(e.hitbox); //solo per vedere l'hitbox
         }
         g2.drawImage(entities.get(0).sprite, entities.get(0).p.x, entities.get(0).p.y, null);
 
