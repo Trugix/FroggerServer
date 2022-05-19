f4917b47cc7218f343d1c5a80e57a36368758ce8
E343C <e.cesari@studenti.unibs.it>
2022-05-19 18:45:44 +0200

    versione finale 1.2

--- src/Server.java
+++ src/Server.java
@@ -13,7 +13,12 @@ import java.net.Socket;
 
 public class Server
 {
-	private static final String ERROR ="ERRORE NELLA TRASMISSIONE DEI DATI TRA CLIENT E SERVER";
+	private static final String ERROR = "ERRORE NELLA TRASMISSIONE DEI DATI TRA CLIENT E SERVER";
+	private static final String INIZIALIZATION = "Inizializzazione del server...";
+	private static final String CONNECTION_YES = "Connessione riuscita";
+	private static final String CONNECTION_NO = "CONNESSIONE NON RIUSCITA";
+	private static final String NOME_FINESTRA = "Avversario";
+	
 	private ObjectInputStream in;
 	private ObjectOutputStream out;
 	private FroggerModel clientModel = new FroggerModel(0);  //crea un'istanza di model che verrà usata per la finestra del client
@@ -55,18 +60,18 @@ public class Server
 					if (first)  //nella prima iterazione crea il nuovo panel e la nuova finestra, inizializzandola allo stato GAME
 					{
 						clientView = new PnlFrog(clientModel);
-						clientView.setState( PnlFrog.STATE.GAME);
-						first=false;
+						clientView.setState(PnlFrog.STATE.GAME);
+						first = false;
 						newWindow();
 					}
 					clientView.setEntities(clientModel.getEntities());
-					if (clientModel.getFrog().getVite()<=0 || clientModel.getDestinazioni()==5) //se l'avversario finisce le vite o vince il suo panel passa a GAME_OVER e il suo punteggio viene salvato
+					if (clientModel.getFrog().getVite() <= 0 || clientModel.getDestinazioni() == 5) //se l'avversario finisce le vite o vince il suo panel passa a GAME_OVER e il suo punteggio viene salvato
 					{
 						clientView.setState(PnlFrog.STATE.GAME_OVER);
 						ctrl.getModel().setPunteggioAvversario(statoClient.getPunteggio()); //aggiorna la variabile usata per calcolare chi ha vinto alla fine del gioco
 						stop = true;
 					}
-					if(ctrl.getFrogView().getState()== PnlFrog.STATE.GAME_OVER && clientView.getState()== PnlFrog.STATE.GAME_OVER ) //se entrambi i giocatori sono a GAME_OVER allora si passa
+					if (ctrl.getFrogView().getState() == PnlFrog.STATE.GAME_OVER && clientView.getState() == PnlFrog.STATE.GAME_OVER) //se entrambi i giocatori sono a GAME_OVER allora si passa
 					{                                                                                                               //alla schermata finale
 						ctrl.getFrogView().setState(PnlFrog.STATE.GAME_OVER_MULTI);
 						ctrl.getFrogView().repaint();
@@ -91,13 +96,13 @@ public class Server
 		try
 		{
 			int porta = 1234;
-			System.out.println("\nDati per la connessione:\nIP: " + InetAddress.getLocalHost().getHostAddress() + "\tPORTA: "+ porta +"\n");
-			System.out.println("Inizializzazione del server...");
+			System.out.println("\nDati per la connessione:\nIP: " + InetAddress.getLocalHost().getHostAddress() + "\tPORTA: " + porta + "\n");
+			System.out.println(INIZIALIZATION);
 			ServerSocket server = new ServerSocket(porta);
 			System.out.println("Server in ascolto sulla porta " + porta);
 			Socket socketClient = server.accept();
 			ctrl.startGame();
-			System.out.println("Connessione riuscita");
+			System.out.println(CONNECTION_YES);
 			server.close();
 			InputStream inputStream = socketClient.getInputStream();
 			OutputStream outputStream = socketClient.getOutputStream();
@@ -107,7 +112,7 @@ public class Server
 		}
 		catch (IOException e)
 		{
-			System.out.println("CONNESSIONE NON RIUSCITA");
+			System.out.println(CONNECTION_NO);
 			e.printStackTrace();
 		}
 	}
@@ -121,7 +126,7 @@ public class Server
 		clientFrame.setBounds(750, 0, 493, 750);
 		clientFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		clientFrame.setLayout(new BorderLayout());
-		clientFrame.setTitle("Avversario");
+		clientFrame.setTitle(NOME_FINESTRA);
 		
 		JPanel mainPanel = new JPanel();
 		mainPanel.setBackground(Color.WHITE);
@@ -138,14 +143,14 @@ public class Server
 	public void send()
 	{
 		Transfer statoCorrente = ctrl.modelToTransfer(ctrl.getModel()); //transforma il model attuale in un trasfer da scriver in output
-        try
-        {
-            out.writeObject(statoCorrente);
-            out.reset();
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
+		try
+		{
+			out.writeObject(statoCorrente);
+			out.reset();
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
 }
